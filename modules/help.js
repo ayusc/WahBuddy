08794074ee2f10ef94eec53d02710aa417be0e10
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-07-05 06:40:10 +0530

    Update help.js

--- modules/help.js
+++ modules/help.js
@@ -14,7 +14,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { getAllCommands } from '../loader.js';
+import { getAllCommands } from '../main.js';
 
 export default {
   name: ['.help'],
