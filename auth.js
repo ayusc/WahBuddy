205ee54a45219441198c30622e324701c408ac71
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-11-06 10:53:51 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,19 @@
+//  WahBuddy - A simple whatsapp userbot written in pure js
+//  Copyright (C) 2025-present Ayus Chatterjee
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import http from 'http';
 import { Server } from 'socket.io';
 import fs from 'node:fs';
@@ -157,9 +173,12 @@ export function initAuth(getLoggedInState) {
   });
 
   app.get('/', (req, res) => {
-    if (getLoggedInState()) {
-      return res.status(200).send('Already logged in!');
-    }
-    res.sendFile(path.join(__dirname, 'public', 'index.al.html'));
-  });
+  // Dynamically re-evaluate the loggedIn state each time
+  const isLoggedIn = typeof getLoggedInState === 'function' ? getLoggedInState() : false;
+
+  if (isLoggedIn) {
+    return res.status(200).send('Already logged in!');
+  }
+  res.sendFile(path.join(__dirname, 'public', 'index.al.html'));
+  });
 }
