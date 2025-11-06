0b69f9ffbd8932f83913236bfdbd30af7d1685a9
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-11-06 21:14:40 +0530

    Update purge.js

--- modules/purge.js
+++ modules/purge.js
@@ -1,3 +1,19 @@
+//  WahBuddy - A simple whatsapp userbot written in pure js
+//  Copyright (C) 2025-present Ayus Chatterjee
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import { messagesCollection } from '../main.js';
 
 const wait = ms => new Promise(r => setTimeout(r, ms));
@@ -5,10 +21,7 @@ const wait = ms => new Promise(r => setTimeout(r, ms));
 async function deleteForEveryone(sock, jid, key) {
   try {
     await sock.sendMessage(jid, { delete: key });
-    return { success: true, key };
-  } catch (err) {
-    return { success: false, error: err, key };
-  }
+  } catch {}
 }
 
 async function deleteForMe(sock, jid, msg) {
@@ -27,17 +40,14 @@ async function deleteForMe(sock, jid, msg) {
       },
       jid
     );
-    return { success: true, key: msg.key };
-  } catch (err) {
-    return { success: false, error: err, key: msg.key };
-  }
+  } catch {}
 }
 
 export default {
   name: ['.purge'],
   description: 'Deletes a replied message and optionally following messages.',
   usage:
-    '.purge [count|all]\nReply to a message then type .purge, .purge n or .purge all.',
+    '.purge [count|all]\nReply to a message and type .purge, .purge n, or .purge all.',
 
   async execute(msg, args, sock) {
     const jid = msg.key.remoteJid;
@@ -65,7 +75,7 @@ export default {
     }
 
     const purgeAll = args[0] === 'all';
-    const count = purgeAll ? 1000 : parseInt(args[0] || '1') + 1;
+    const purgeCount = purgeAll ? 1000 : parseInt(args[0] || '1') + 1;
 
     const messagesToDelete = await messagesCollection
       .find({
@@ -73,17 +83,14 @@ export default {
         messageTimestamp: { $gte: repliedMsg.messageTimestamp }
       })
       .sort({ messageTimestamp: 1 })
-      .limit(count)
+      .limit(purgeCount)
       .toArray();
 
-    // add the command message itself
     messagesToDelete.push(msg);
 
     for (const m of messagesToDelete) {
-      const result = await deleteForEveryone(sock, jid, m.key);
-      if (!result.success) {
-        await deleteForMe(sock, jid, m);
-      }
+      await deleteForEveryone(sock, jid, m.key);
+      await deleteForMe(sock, jid, m);
       await wait(500);
     }
 
