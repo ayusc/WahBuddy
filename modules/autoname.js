4c11017d24c637e447718b7311ae9d86e038cc1a
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-12-06 16:27:51 +0530

    Prevent multiple instances of startAutoName

--- modules/autoname.js
+++ modules/autoname.js
@@ -59,6 +59,7 @@ async function performNameUpdate() {
 }
 
 export async function startAutoName() {
+	if (globalThis.autonameRunning) return;
 	globalThis.autonameRunning = true;
 
 	const runRecursiveLoop = async () => {
